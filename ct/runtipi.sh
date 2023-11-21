bd4769ddc108063921c9eb640658cfc22d98e538
DrMxrcy <drmxrcy@gmail.com>
2023-11-21 17:29:05 -0500

    Update runtipi.sh

--- ct/runtipi.sh
+++ ct/runtipi.sh
@@ -66,4 +66,4 @@ description
 
 msg_ok "Completed Successfully!\n"
 echo -e "${APP} Setup should be reachable by going to the following URL.
-         ${BL}http://${IP} ${CL} \n"
+         ${BL}http://${IP}${CL} \n"
